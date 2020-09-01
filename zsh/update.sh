18a8d97c5d9d7ef5184a24c48dd079c4ef801511
Stanley Xu <16456356+stanley-xu@users.noreply.github.com>
2020-09-01 14:12:17 -0400

    zsh: cleanup

--- zsh/update.sh
+++ zsh/update.sh
@@ -1,57 +1,53 @@
 #!/usr/bin/env bash
 
 STARTER_ZSHRC="zshrc"
-ZSH_DIR="zsh"
-DST_ZSH_DIR="${HOME}/.${ZSH_DIR}" # the "destination" zsh directory
+STARTER_ZSH_HOME="zsh"
+DST_ZSH_HOME="${HOME}/.${STARTER_ZSH_HOME}" # the "destination" zsh directory
 DST_ZSHRC="${HOME}/.${STARTER_ZSHRC}" # the "destination" zshrc
 
 function update_all {
-  cd ${DST_ZSH_DIR}
+  cd ${DST_ZSH_HOME}
 
-  echo "OMZ's completion"
+  out "OMZ's completion"
   curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/completion.zsh -o completion.zsh
-  echo ''
 
-  echo "OMZ's history"
+  out "OMZ's history"
   curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/history.zsh -o history.zsh
-  echo ''
 
-  echo 'zsh-autosuggestions'
+  out 'zsh-autosuggestions'
   update_repo zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions.git
+
+  out 'powerlevel10k...'
+  update_repo powerlevel10k https://github.com/romkatv/powerlevel10k.git
+}
+
+function out {
+  echo ''
+  echo $1
 }
 
 function update_repo {
-  REPO=$1 # zsh-autosuggestions
+  REPO="${DST_ZSH_HOME}/$1" # zsh-autosuggestions
   LINK=$2 # https://github.com/zsh-users/zsh-autosuggestions.git
 
   if [ ! -d ${REPO} ]; then
-    git clone ${LINK}
+    git clone --depth=1 ${LINK} ${REPO}
   else
     git -C ${REPO} pull
   fi
-}
-
-function add_p10k {
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-  echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 }
 
 function run {
   cd "$(dirname "$0")"
 
   # Create starter config
-  if [ ! -d ${DST_ZSH_DIR} ]; then
-    echo "${DST_ZSH_DIR} not found"
+  if [ ! -d ${DST_ZSH_HOME} ]; then
+    out "${DST_ZSH_HOME} not found"
 
-    if [ -d "$HOME/powerlevel10k" ]; then
-      echo 'Adding powerlevel10k...'
-      add_p10k
-    fi
-    
-    echo 'Copying starter .zsh/ and .zshrc...'
-    cp ${STARTER_ZSHRC} ${DST_ZSHRC} && cp -r ${ZSH_DIR} ${DST_ZSH_DIR}
+    out 'Copying starter .zsh/ and .zshrc...'
+    cp ${STARTER_ZSHRC} ${DST_ZSHRC} && cp -r ${STARTER_ZSH_HOME} ${DST_ZSH_HOME}
   else
-    echo 'Updating external scripts...'
+    out 'Updating external scripts...'
   fi
 
   update_all
@@ -59,4 +55,4 @@ function run {
 
 run
 
-echo 'Done'  
+out 'Done'  
