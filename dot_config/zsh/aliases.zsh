5227a2fff2086685099d538438635ed34b20b065
Stanley Xu <stanley.xuu@gmail.com>
2025-07-18 13:52:10 -0400

    Minify git aliases

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -6,81 +6,47 @@ alias la='ls -a'
 alias lla='ls -la'
 
 # Git
+# Key workflows
+# - git concepts: working -> staging -> remote
+# - edit: commit, rebase, fetch / pull, push
+# - info: status, remote, diff, branch
 alias g='git'
-alias gst='git status'
 
-## Commit (c)
-alias gc='git commit --verbose'
+## Info
+alias gst='git status'
+alias ga='git add'
+alias gd='git diff'
+alias gco='git checkout'
+alias gre='git reset'
+alias gl='git log'
+alias glo='git log --pretty=oneline --abbrev-commit'
+alias glg='git log --graph --decorate --pretty=oneline --abbrev-commit'
+
+## Edit
+### Commit (c)
+alias gc='git commit --verbose' # diff is shown in editor
 alias gca='git commit --verbose --all'
 alias gcm='git commit --message'
 alias gcam='git commit --all --message'
-alias gco='git checkout'
-alias gcf='git commit --amend --reuse-message HEAD'
-alias gcF='git commit --verbose --amend'
+alias gcr='git commit --amend --reuse-message HEAD'
 alias gcp='git cherry-pick --ff'
-alias gcP='git cherry-pick --no-commit'
-alias gcr='git revert'
 alias gcR='git reset "HEAD^"'
-alias gcs='git show'
-alias gcsS='git show --pretty=short --show-signature'
 
-## Fetch (f)
-alias gf='git fetch'
-alias gfa='git fetch --all'
-alias gfc='git clone'
-alias gfm='git pull'
-alias gfr='git pull --rebase'
-
-## Index (i)
-alias gia='git add'
-alias giA='git add --patch'
-alias giu='git add --update'
-alias gid='git diff --no-ext-diff --cached'
-alias giD='git diff --no-ext-diff --cached --word-diff'
-alias gir='git reset'
-alias giR='git reset --patch'
-alias gix='git rm -r --cached'
-alias giX='git rm -rf --cached'
-
-## Log (l)
-alias gl='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
-alias gls='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
-alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"${_git_log_medium_format}"'
-alias glo='git log --topo-order --pretty=format:"${_git_log_oneline_format}"'
-alias glg='git log --topo-order --graph --pretty=format:"${_git_log_oneline_format}"'
-alias glb='git log --topo-order --pretty=format:"${_git_log_brief_format}"'
-
-## Push (p)
-alias gp='git push'
-alias gpf='git push --force-with-lease'
-alias gpF='git push --force'
-alias gpt='git push --tags'
-alias gpc='git push --set-upstream origin "$(git-branch-current 2> /dev/null)"'
-alias gpp='git pull origin "$(git-branch-current 2> /dev/null)" && git push origin "$(git-branch-current 2> /dev/null)"'
-
-## Rebase (r)
+### Rebase (r)
 alias gr='git rebase'
 alias gra='git rebase --abort'
 alias grc='git rebase --continue'
 alias gri='git rebase --interactive'
 alias grs='git rebase --skip'
 
-## Stash (s)
-alias gs='git stash'
-alias gsa='git stash apply'
-alias gsx='git stash drop'
-alias gsl='git stash list'
-alias gsd='git stash show --patch --stat'
-alias gsp='git stash pop'
+### Fetch (f)
+alias gf='git fetch'
+alias gff='git pull --ff-only'
+alias gfr='git pull --rebase'
 
-## Working Copy (w)
-alias gws='git status --ignore-submodules=${_git_status_ignore_submodules} --short'
-alias gwS='git status --ignore-submodules=${_git_status_ignore_submodules}'
-alias gwd='git diff --no-ext-diff'
-alias gwD='git diff --no-ext-diff --word-diff'
-alias gwr='git reset --soft'
-alias gwR='git reset --hard'
-alias gwc='git clean -n'
-alias gwC='git clean -f'
-alias gwx='git rm -r'
-alias gwX='git rm -rf'
+### Push (p)
+alias gp='git push'
+alias gpf='git push --force-with-lease'
+alias gpF='git push --force'
+alias gpu='git push --set-upstream origin "$(git branch --show-current 2> /dev/null)"'
+alias gpp='git pull origin "$(git branch --show-current 2> /dev/null)" && git push origin "$(git branch --show-current 2> /dev/null)"'
